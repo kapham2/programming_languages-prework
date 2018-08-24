8c6032838b222fb686568e9080bd5e7a40d4b749
Kim Pham <pham.kim.a@gmail.com>
2018-08-24 15:05:31 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,47 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  
+  languages.each do |style, language_hash|
+    language_hash.each do |lang, type_hash|
+      type_hash.each do|type, value|
+        new_hash[lang] = {type => value, :style => [style]}
+      end
+    end
+  end
+
+  puts new_hash
+  new_hash
 end
+
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+ 
+  }
+}
+reformat_languages(languages)
